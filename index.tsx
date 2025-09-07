16f21cb83b41683c5054a4f94ae230ce4973db45
Jerry <maevehuang10@gmail.com>
2025-09-07 11:33:21 +0800

    添加空行以改善代码可读性

--- index.tsx
+++ index.tsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import SnakeGame from './SnakeGame';
 
+
+
+
 const root = createRoot(document.getElementById('root')!);
 root.render(<SnakeGame />);
