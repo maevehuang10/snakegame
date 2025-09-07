91570acd05fa8263d22f1f101fb0f9257be9d559
Jerry <maevehuang10@gmail.com>
2025-09-07 13:50:15 +0800

    删除所有文件

--- index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import SnakeGame from './SnakeGame';
-
-
-
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<SnakeGame />);
